05012989e3b8f21bc65c2bd21314a7d040249fee
komod0 <juliancrespo15@gmail.com>
2017-10-26 22:27:51 -0300

    Creado el TDA Heap. - Agregado heap_crear, heap_destruir, esta_vacio, heap_cantidad - No estoy seguro si destruir va a funcionar bien
    Signed-off-by: komod0 <juliancrespo15@gmail.com>

---
+++ 8-Heap/heap.c
@@ -0,0 +1,46 @@
+#include "heap.h"
+
+#define TAM_INICIAL 32
+
+struct heap {
+    cmp_func_t comparar;
+    void** arreglo;
+    size_t tamanio;
+    size_t cantidad;
+};
+
+heap_t *heap_crear(cmp_func_t cmp) {
+    heap_t* heap = malloc(sizeof(heap_t));
+    if (heap == NULL) return NULL;
+
+    heap->arreglo = malloc(sizeof(void*) * TAM_INICIAL);
+    if (heap->arreglo == NULL) {
+        free(heap);
+        return NULL;
+    }
+
+    heap->comparar = cmp;
+    heap->tamanio = TAM_INICIAL;
+    heap->cantidad = 0;
+
+    return heap;
+}
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
+    if (destruir_elemento) {
+        for (int i = 0; i < heap->cantidad; ++i) {
+            destruir_elemento(heap->arreglo[i]);
+        }
+    }
+    free(heap->arreglo);
+    free(heap);
+
+}
+
+size_t heap_cantidad(const heap_t *heap) {
+    return heap->cantidad;
+}
+
+bool heap_esta_vacio(const heap_t *heap) {
+    return heap->cantidad == 0;
+}
