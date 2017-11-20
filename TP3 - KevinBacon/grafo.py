6cf8fb0abc655c08bf6dcbfbb8007e332f122fdf
Lautis1 <lautaro.stroia@gmail.com>
2017-11-20 02:36:06 -0300

    Agregadas algunas funciones de grafo y tp

--- TP3 - KevinBacon/grafo.py
+++ TP3 - KevinBacon/grafo.py
@@ -21,25 +21,67 @@ class Grafo:
 		como origen o destino"""
 		if not vertice_a_borrar in self.vertices:
 			return
-		for vertice_ady in self.vertices[vertice_a_borrar].keys():
-			del(self.vertices[vertice_ady])
-		del(self.vertices[vertice_a_borrar])
+		for vertice_ady in self.vertices:
+			self.vertices[vertice_ady].pop(vertice_a_borrar)
+		self.vertices.pop(vertice_a_borrar)
 
 	def agregar_arista(self, vertice_origen, vertice_destino, nombre):
 		"""Une los vertices recibidos mediante una arista a crear, cuyo nombre es
 		el nombre recibido"""
 
-		self.vertices[vertice_origen] = Arista(vertice_origen, vertice_destino, nombre)
+		self.vertices[vertice_origen] = Arista(vertice_origen, vertice_destino,nombre)
+		#Sin clase arista seria algo asi CREO
+		self.vertices[vertice_origen][vertice_destino] = nombre
+		self.vertices[vertice_destino][vertice_origen] = nombre
 
 
-class Arista:
+	def eliminar_arista(self, vertice_origen, vertice_destino):
+		"""Borra la arista que une los dos vertices recibidos por parametro"""
+		
+		if not (vertice_origen or vertice_destino) in self.vertices:
+			return
+		self.vertices[vertice_origen].pop(vertice_destino)
+		self.vertices[vertice_destino].pop(vertice_origen)
 
-	def __init__(self, v_origen, v_destino, nombre_arista):
+	def obtener_adyacentes(self, vertice):
+		"""Devuelve una lista de todos los vertices adyacentes al vertice recibido por
+		parametro."""
+
+		lista_vertices_ady = []
+		for ady in self.vertices[vertice]:
+			lista_vertices_ady.append(ady)
+		return lista_vertices_ady
+
+	def chequear_adyacencia(self, vertice1, vertice2):
+		"""Devuelve un booleano segun si los vertices dados son adyacentes o no"""
+
+		return vertice1 in self.vertices[vertice2] and vertice2 in self.vertices[vertice1]
+
+	def obtener_vertices(self):
+		"""Devuelve una lista de todos los vertices presentes en el grafo"""
+		return self.vertices.keys()
+
+	def obtener_aristas(self, vertice1, vertice2):
+		"""Devuelve la informacion que contiene la arista que une dos vertices"""
+		return self.vertices[vertice1][vertice2] #sin usar clase arista
+		#usando clase arista seria algo asi creo
+		arista = Arista(vertice1,vertice2)
+		return arista.nombre_arista
+
+	def __str__(self):
+
+		vertices = self.vertices
+		return str(vertices)
+
+#################################################################################3
+class Arista: #Hice esta clase xq fede dijo que era recomendable
+
+	def __init__(self, v_origen, v_destino, nombre = None):
 		"""Constructor de una Arista que une a los vertices recibidos.
 		Dicha arista tiene nombre."""
 		self.v_origen = v_origen
 		self.v_destino = v_destino
-		self.nombre_arista = nombre_arista
+		self.nombre_arista = nombre
 
 		#Uno los vertices
 		self.vertices[v_origen][v_destino] = nombre_arista
--- TP3 - KevinBacon/libreria_funciones.py
+++ TP3 - KevinBacon/libreria_funciones.py
@@ -1,7 +1,9 @@
 #LIBRERIA DE FUNCIONES DE GRAFO PARA IMPLEMENTAR PARA TP3
 
 from collections import deque
-import grafo
+from grafo import *
+import csv
+
 ESTADO_VISITADO = True
 ESTADO_NO_VISITADO = False
 
@@ -17,9 +19,9 @@ def BreathFirstSearch(grafo, vertice_origen):
 	vertices_visitados[vertice_origen] = ESTADO_VISITADO
 	padre_vertice[vertice_origen] = None
 	orden_vertice[vertice_origen] = 0
-	while not cola.__len() == 0:
+	while not len(cola) == 0:
 		nuevo_vertice = cola.pop()
-		for v_ady in grafo.vertices[nuevo_vertice].keys():
+		for v_ady in grafo.obtener_adyacentes(nuevo_vertice):
 			if vertices_visitados[v_ady] == ESTADO_NO_VISITADO:
 				vertices_visitados[v_ady] = ESTADO_VISITADO
 				padre_vertice[v_ady] = nuevo_vertice
@@ -32,7 +34,7 @@ def DepthFirstSearch(grafo):
 	vertices_visitados = {}
 	padre_vertice = {}
 	orden_vertice = {}
-	for vertice in grafo.vertices:
+	for vertice in grafo.obtener_vertices():
 		if vertices_visitados[vertice] == ESTADO_NO_VISITADO:
 			padre_vertice[vertice] = None
 			orden_vertice[vertice] = 0
@@ -42,8 +44,61 @@ def DepthFirstSearch(grafo):
 def dfs_visitar(grafo,origen, dicc_vertices_visitados, dicc_padres, dicc_orden):
 
 	dicc_vertices_visitados[origen] = ESTADO_VISITADO
-	for v_ady in grafo.vertices[origen].keys():
+	for v_ady in grafo.obtener_adyacentes(origen):
 		if dicc_vertices_visitados[v_ady] == ESTADO_NO_VISITADO:
 			dicc_padres[v_ady] = origen
 			dicc_orden[v_ady] = dicc_orden[origen] + 1
 			dfs_visitar(grafo,v_ady,dicc_vertices_visitados,dicc_padres,dicc_orden)
+
+#CAMINO MINIMO = DIJKSTRA
+
+def camino_minimo(grafo, origen, destino):
+
+	"""Recorre el grafo con BFS para buscar el camino minimo entre dos vertices.
+	Devuelve una lista con el camino, None en caso que no haya camino."""
+
+	cola = deque()
+	vertices_visitados = {}
+	padre_vertices = {}
+	#lista_a_devolver = []
+
+	#Recorro cada vertice del grafo y lo marco como NO visitado, y sin padre
+	for vertice in grafo.obtener_vertices():
+		vertices_visitados[vertice] = ESTADO_NO_VISITADO
+		padre_vertices[vertice] = None
+	cola.append(origen)
+	vertices_visitados[origen] = ESTADO_VISITADO
+	padre_vertices[origen] = None
+	while not len(cola) == 0:
+		v_actual = cola.pop()
+		vertices_visitados[v_actual] = ESTADO_VISITADO
+		for v_ady in grafo.obtener_adyacentes(v_actual):
+			if not v_ady in vertices_visitados:
+				vertices_visitados[v_ady] = ESTADO_VISITADO
+				padre_vertices[v_ady] = v_actual
+				cola.append(v_ady)
+	return padre_vertices
+
+
+#############################################
+def crear_grafo(archivo):
+	"""Crea un grafo a partir de las lineas de un archivo csv"""
+	grafo = Grafo()
+	with open(archivo,'r') as actores:
+		lector = csv.reader(actores)
+		for linea in lector:
+			actor = linea[0]
+			grafo.agregar_vertice(actor)
+	return grafo
+	
+
+grafo = crear_grafo("actores.csv")
+print(grafo)
+
+
+
+
+
+
+
+
