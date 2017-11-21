4c3d1c4d90126f2bf4fe37db4af0ce49ffe0cd7b
Lautaro <lautaro.stroia@gmail.com>
2017-11-21 00:37:07 -0300

    todo igual

--- TP3 - KevinBacon/grafo.py
+++ TP3 - KevinBacon/grafo.py
@@ -1,38 +1,44 @@
 ##IMPLEMENTACION DE LA CLASE GRAFO.
 #EMPIEZO COMO DICCIONARIO DE DICCIONARIOS, SI PREFERIS OTRA COSA MODIFICAMOS
 
+
+####################################################################################
 class Grafo:
 
 	"""Inicializacion de la clase Grafo"""
 	def __init__(self):
 		"""Constructor de un grafo vacio. Crea un diccionario de vertices"""
+		self.cantidad_vertices = 0
 		self.vertices = {}
+		self.cantidad_aristas = 0
 
 	def agregar_vertice(self, nombre_vertice):
 		"""Recibe el grafo y un vertice. Si el vertice no se encuentra en el grafo, 
 		lo agrega."""
-		if nombre_vertice in self.vertices:
-			return
+		if  nombre_vertice in self.vertices:
+			return False
 		self.vertices[nombre_vertice] = {}
+		self.cantidad_vertices += 1
+		return True
 
 	def eliminar_vertice(self, vertice_a_borrar):
 		"""Borra el vertice 'vertice_a_borrar' del grafo.
 		Ademas, hay que eliminar las aristas que tuviesen al vertice borrado
 		como origen o destino"""
 		if not vertice_a_borrar in self.vertices:
-			return
+			return False
 		for vertice_ady in self.vertices:
 			self.vertices[vertice_ady].pop(vertice_a_borrar)
+			self.cantidad_aristas -= 1
 		self.vertices.pop(vertice_a_borrar)
+		self.cantidad_vertices -= 1
 
 	def agregar_arista(self, vertice_origen, vertice_destino, nombre):
 		"""Une los vertices recibidos mediante una arista a crear, cuyo nombre es
 		el nombre recibido"""
 
 		self.vertices[vertice_origen] = Arista(vertice_origen, vertice_destino,nombre)
-		#Sin clase arista seria algo asi CREO
-		self.vertices[vertice_origen][vertice_destino] = nombre
-		self.vertices[vertice_destino][vertice_origen] = nombre
+		self.cantidad_aristas += 1
 
 
 	def eliminar_arista(self, vertice_origen, vertice_destino):
@@ -42,8 +48,9 @@ class Grafo:
 			return
 		self.vertices[vertice_origen].pop(vertice_destino)
 		self.vertices[vertice_destino].pop(vertice_origen)
+		self.cantidad_aristas -= 1
 
-	def obtener_adyacentes(self, vertice):
+	def obtener_adyacentes(self, vertice_padre):
 		"""Devuelve una lista de todos los vertices adyacentes al vertice recibido por
 		parametro."""
 
@@ -55,11 +62,13 @@ class Grafo:
 	def chequear_adyacencia(self, vertice1, vertice2):
 		"""Devuelve un booleano segun si los vertices dados son adyacentes o no"""
 
-		return vertice1 in self.vertices[vertice2] and vertice2 in self.vertices[vertice1]
+		verticeA = self.vertices[vertice1]
+		verticeB = self.vertices[vertice2]
+		return verticeA in verticeB and verticeB in verticeA
 
 	def obtener_vertices(self):
 		"""Devuelve una lista de todos los vertices presentes en el grafo"""
-		return self.vertices.keys()
+		return self.vertices
 
 	def obtener_aristas(self, vertice1, vertice2):
 		"""Devuelve la informacion que contiene la arista que une dos vertices"""
