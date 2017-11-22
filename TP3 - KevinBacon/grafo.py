47b0434ab501f8152e1f479e851837e2c033ac67
Lautaro <lautaro.stroia@gmail.com>
2017-11-21 21:05:38 -0300

    modificado crear grafo

--- TP3 - KevinBacon/grafo.py
+++ TP3 - KevinBacon/grafo.py
@@ -16,11 +16,10 @@ class Grafo:
 		"""Recibe el grafo y un vertice. Si el vertice no se encuentra en el grafo, 
 		lo agrega."""
 		if  nombre_vertice in self.vertices:
-			return False
+			return
 		self.vertices[nombre_vertice] = {}
 		self.cantidad_vertices += 1
-		return True
-
+		
 	def eliminar_vertice(self, vertice_a_borrar):
 		"""Borra el vertice 'vertice_a_borrar' del grafo.
 		Ademas, hay que eliminar las aristas que tuviesen al vertice borrado
@@ -37,7 +36,8 @@ class Grafo:
 		"""Une los vertices recibidos mediante una arista a crear, cuyo nombre es
 		el nombre recibido"""
 
-		self.vertices[vertice_origen] = Arista(vertice_origen, vertice_destino,nombre)
+		self.vertices[vertice_origen][vertice_destino] = nombre
+		self.vertices[vertice_destino][vertice_origen] = nombre
 		self.cantidad_aristas += 1
 
 
@@ -77,6 +77,14 @@ class Grafo:
 		arista = Arista(vertice1,vertice2)
 		return arista.nombre_arista
 
+	def cantidad_vertices(self):
+		"""Devuelve la cantidad de vertices presentes en el grafo"""
+		return self.cantidad_vertices
+
+	def cantidad_aristas(self):
+		"""Devuelve la cantidad de aristas presentes en el grafo"""
+		return self.cantidad_aristas
+		
 	def __str__(self):
 
 		vertices = self.vertices
@@ -85,16 +93,17 @@ class Grafo:
 #################################################################################3
 class Arista: #Hice esta clase xq fede dijo que era recomendable
 
-	def __init__(self, v_origen, v_destino, nombre = None):
+	def __init__(self,v_origen, v_destino, nombre):
 		"""Constructor de una Arista que une a los vertices recibidos.
 		Dicha arista tiene nombre."""
 		self.v_origen = v_origen
 		self.v_destino = v_destino
 		self.nombre_arista = nombre
+		vertices = grafo.vertices
 
 		#Uno los vertices
-		self.vertices[v_origen][v_destino] = nombre_arista
-		self.vertices[v_destino][v_origen] = nombre_arista
+		vertices[v_origen][v_destino] = nombre
+		vertices[v_destino][v_origen] = nombre
 
 	def nombre_arista(self, arista):
 
--- TP3 - KevinBacon/libreria_funciones.py
+++ TP3 - KevinBacon/libreria_funciones.py
@@ -29,6 +29,7 @@ def BreathFirstSearch(grafo, vertice_origen):
 				cola.append(v_ady)
 
 
+
 def DepthFirstSearch(grafo):
 
 	vertices_visitados = {}
@@ -60,7 +61,7 @@ def camino_minimo(grafo, origen, destino):
 	cola = deque()
 	vertices_visitados = {}
 	padre_vertices = {}
-	#lista_a_devolver = []
+	orden_vertices = {}
 
 	#Recorro cada vertice del grafo y lo marco como NO visitado, y sin padre
 	for vertice in grafo.obtener_vertices():
@@ -69,33 +70,102 @@ def camino_minimo(grafo, origen, destino):
 	cola.append(origen)
 	vertices_visitados[origen] = ESTADO_VISITADO
 	padre_vertices[origen] = None
-	while not len(cola) == 0:
+	orden_vertices[origen] = 0
+	while len(cola) > 0:
 		v_actual = cola.pop()
-		vertices_visitados[v_actual] = ESTADO_VISITADO
+		if v_actual == destino: return padre_vertices,orden_vertices
 		for v_ady in grafo.obtener_adyacentes(v_actual):
 			if not v_ady in vertices_visitados:
 				vertices_visitados[v_ady] = ESTADO_VISITADO
 				padre_vertices[v_ady] = v_actual
+				orden_vertices[v_ady] = orden_vertices[v_actual] + 1
 				cola.append(v_ady)
-	return padre_vertices
+	return padre_vertices,orden_vertices
 
 
 #############################################
-def crear_grafo(archivo):
-	"""Crea un grafo a partir de las lineas de un archivo csv"""
-	grafo = Grafo()
-	with open(archivo,'r') as actores:
-		lector = csv.reader(actores)
-		for linea in lector:
-			actor = linea[0]
-			grafo.agregar_vertice(actor)
-	return grafo
-	
 
-grafo = crear_grafo("actores.csv")
-print(grafo)
+def grafo_crear(nombre_archivo):
+    """
+    Crea un grafo de conexiones de actores a partir de un archivo de datos.
+
+    PRE: Recibe el nombre de un archivo separado por comas que contenga de lineas:
+        actor,pelicula,pelicula,pelicula
+        que equivalen a: vertice,arista,arista,arista
+    POST: Devuelve un grafo creado a partir de estos datos.
+    """
+    grafo = Grafo()
+    dicc_pelis = {}
+    lista_actores = []
+    with open(nombre_archivo) as actores:
+    	lector = csv.reader(actores)
+    	for linea in lector:
+    		actor = linea[0]
+    		peliculas = linea[1:]
+    		grafo.agregar_vertice(actor)
+    		for peli in peliculas:
+    			if not peli in dicc_pelis:
+    				lista_actores.append(actor)
+    				dicc_pelis[peli] = lista_actores
+    			else:
+    				dicc_pelis[peli] += actor
+    			for i in range(len(lista_actores)-1):
+    				grafo.agregar_arista(lista_actores[i], lista_actores[i+1], peli)
+    return grafo
+    
+    #raise NotImplementedError
+dicc = grafo_crear("test.csv")
+print(dicc)
+
+#print(dicc_peliculas)
+
+'''def camino(grafo, origen, llegada):
+    """
+    Devuelve el camino entre un actor de origen y uno de llegada.
+
+    PRE: Recibe el grafo, un actor de origen y un actor de llegada.
+    POST: Devuelve una lista ordenada de cadenas (películas) para llegar desde
+        el origen hasta el final.
+    """
+
+    padres_v, orden_v = camino_minimo(grafo,origen,llegada)
+    recorrido_minimo = []
+    v_actual = padres_v.get(llegada) #Devuelve ady de "llegada" o None si no tiene
+
+    raise NotImplementedError
 
 
+def actores_a_distancia(grafo, origen, n):
+    """
+    Devuelve los actores a distancia n del recibido.
+
+    PRE: Recibe el grafo, el actor de origen y el número deseado.
+    POST: Devuelve la lista de cadenas (actores) a n pasos del recibido.
+    """
+    raise NotImplementedError
+
+
+def popularidad(grafo, actor):
+    """
+    Calcula la popularidad del actor recibido.
+
+    PRE: Recibe el grafo y un actor de origen
+    POST: Devuelve un entero que simboliza la popularidad: todos los adyacentes
+        de los adyacentes del actor, multiplicado por su cantidad de peliculas
+    """
+    raise NotImplementedError
+
+
+def similares(grafo,origen, n):
+    """
+    Calcula los n actores más similares al actor de origen y los devuelve en una
+    lista ordenada de mayor similitud a menor.
+
+    PRE: Recibe el grafo, el actor de origen, y el n deseado
+    POST: Devuelve una lista de los n actores no adyacentes más similares al
+        pedido. La lista no debe contener al actor de origen.
+    """
+    raise NotImplementedError'''
 
 
 
