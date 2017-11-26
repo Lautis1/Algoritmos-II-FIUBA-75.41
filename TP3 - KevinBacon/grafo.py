be1935066f4084c46a654402e4528ca8a1548237
komod0 <juliancrespo15@gmail.com>
2017-11-26 03:34:04 -0300

    Corregi varias cosas, elimine funciones innecesarias, modularize un toque las funciones de camino por ejemplo. Modifique la funcion de crear_grafo. No teestie nada, mañana ya supongo casi terminariamos si todo va bien.
    Signed-off-by: komod0 <juliancrespo15@gmail.com>

--- TP3 - KevinBacon/grafo.py
+++ TP3 - KevinBacon/grafo.py
@@ -2,63 +2,62 @@ from collections import defaultdict
 
 # ###################################################################################
 
+
 class Grafo:
     """Inicializacion de la clase Grafo"""
 
     def __init__(self):
         """Constructor de un grafo vacio. Crea un diccionario de vertices"""
         self.vertices = {}
-        self.cantidad_aristas = 0
+        # Ahora self.vertices es un diccionario de diccionarios de sets.
+        # Elimine cantidad de aristas, no sirve, cantidad de aristas != cantidad de peliculas.
 
     def agregar_vertice(self, nombre_vertice):
         """Recibe el grafo y un vertice. Si el vertice no se encuentra en el grafo,
         lo agrega."""
         if nombre_vertice in self.vertices:
             return
-        self.vertices[nombre_vertice] = defaultdict(list)
+        self.vertices[nombre_vertice] = defaultdict(set)
 
     def eliminar_vertice(self, vertice_a_borrar):
         """Borra el vertice 'vertice_a_borrar' del grafo.
         Ademas, hay que eliminar las aristas que tuviesen al vertice borrado
         como origen o destino"""
         if vertice_a_borrar not in self.vertices:
-            return False
-        for vertice_ady in self.vertices:
-            self.vertices[vertice_ady].pop(vertice_a_borrar)
-            self.cantidad_aristas -= 1
-        self.vertices.pop(vertice_a_borrar)
+            return
+        for vertice_ady in self.vertices[vertice_a_borrar]:
+            del self.vertices[vertice_ady][vertice_a_borrar]
+        del self.vertices[vertice_a_borrar]
 
     def agregar_arista(self, vertice_origen, vertice_destino, nombre):
         """Une los vertices recibidos mediante una arista a crear, cuyo nombre es
         el nombre recibido"""
 
-        self.vertices[vertice_origen][vertice_destino].append(nombre)
-        self.vertices[vertice_destino][vertice_origen].append(nombre)
-        self.cantidad_aristas += 1
+        self.vertices[vertice_origen][vertice_destino].add(nombre)
+        self.vertices[vertice_destino][vertice_origen].add(nombre)
 
     def eliminar_arista(self, vertice_origen, vertice_destino):
         """Borra la arista que une los dos vertices recibidos por parametro"""
 
-        if not (vertice_origen or vertice_destino) in self.vertices:
+        if vertice_origen not in self.vertices or vertice_destino not in self.vertices:
             return
-        self.vertices[vertice_origen].pop(vertice_destino)
-        self.vertices[vertice_destino].pop(vertice_origen)
-        self.cantidad_aristas -= 1
+        del self.vertices[vertice_origen][vertice_destino]
+        del self.vertices[vertice_destino][vertice_origen]
 
     def obtener_adyacentes(self, vertice_padre):
         """Devuelve una lista de todos los vertices adyacentes al vertice recibido por
         parametro."""
-        if not vertice_padre in self.vertices:
-        	return
+        if vertice_padre not in self.vertices:
+            return
 
-        return [ady for ady in self.vertices[vertice_padre]]
+        return self.vertices[vertice_padre].keys()
 
     def chequear_adyacencia(self, vertice1, vertice2):
         """Devuelve un booleano segun si los vertices dados son adyacentes o no"""
 
-        verticeA = self.vertices[vertice1]
-        verticeB = self.vertices[vertice2]
-        return verticeA in verticeB and verticeB in verticeA
+        # Hace falta el chequeo doble?, el grafo no es dirigido, la relacion es bilateral si vertice1 esta conectado
+        # con vertice2 entonces vertice 2 esta conectado con vertice 1 seguramente.
+        return vertice1 in self.vertices[vertice2]
 
     def obtener_vertices(self):
         """Devuelve una lista de todos los vertices presentes en el grafo"""
@@ -66,36 +65,7 @@ class Grafo:
 
     def obtener_aristas(self, vertice1, vertice2):
         """Devuelve la informacion que contiene la arista que une dos vertices"""
-        return self.vertices[vertice1][vertice2]  # sin usar clase arista
-
-    def cantidad_vertices(self):
-        """Devuelve la cantidad de vertices presentes en el grafo"""
-        return self.cantidad_vertices
-
-    def cantidad_aristas(self):
-        """Devuelve la cantidad de aristas presentes en el grafo"""
-        return self.cantidad_aristas
+        return self.vertices[vertice1][vertice2]
 
     def __str__(self):
-
-        vertices = self.vertices
-        return str(vertices)
-
-
-# ################################################################################
-class Arista:  # Hice esta clase xq fede dijo que era recomendable
-
-    def __init__(self, v_origen, v_destino, nombre):
-        """Constructor de una Arista que une a los vertices recibidos.
-        Dicha arista tiene nombre."""
-        self.v_origen = v_origen
-        self.v_destino = v_destino
-        self.nombre_arista = nombre
-        vertices = grafo.vertices
-
-        # Uno los vertices
-        vertices[v_origen][v_destino] = nombre
-        vertices[v_destino][v_origen] = nombre
-
-    def nombre_arista(self, arista):
-        return self.arista.nombre_arista
+        return str(self.vertices)
--- TP3 - KevinBacon/seis_grados.py
+++ TP3 - KevinBacon/seis_grados.py
@@ -10,80 +10,38 @@ import csv
 
 ESTADO_VISITADO = True
 ESTADO_NO_VISITADO = False
+GRADO_CONSIDERADO_PARA_POPULARIDAD = 2
 
 
 ###################################################################################
 #                            RECORRIDOS EN GRAFO                                  #
 ###################################################################################
 
-def breathfirstsearch(grafo, vertice_origen):
-    """Recorrido del grafo en anchura"""
 
-    vertices_visitados = {}
-    padre_vertice = {}
-    orden_vertice = {}
-    cola = deque()
-    cola.append(vertice_origen)
-    vertices_visitados[vertice_origen] = ESTADO_VISITADO
-    padre_vertice[vertice_origen] = None
-    orden_vertice[vertice_origen] = 0
-    while not len(cola) == 0:
-        nuevo_vertice = cola.popleft()
-        for v_ady in grafo.obtener_adyacentes(nuevo_vertice):
-            if v_ady not in vertices_visitados:
-                vertices_visitados[v_ady] = ESTADO_VISITADO
-                padre_vertice[v_ady] = nuevo_vertice
-                orden_vertice[v_ady] = orden_vertice[nuevo_vertice] + 1
-                cola.append(v_ady)
-    return orden_vertice  # Devuelve esto por conveniencia para usar en actores_a_distancia
-
-
-def depthfirstsearch(grafo):
-    """Recorrido del grafo en profundidad"""
-    vertices_visitados = {}
-    padre_vertice = {}
-    orden_vertice = {}
-    for vertice in grafo.obtener_vertices():
-        if vertices_visitados[vertice] == ESTADO_NO_VISITADO:
-            padre_vertice[vertice] = None
-            orden_vertice[vertice] = 0
-            dfs_visitar(grafo, vertice, vertices_visitados, padre_vertice, orden_vertice)
-
-
-def dfs_visitar(grafo, origen, dicc_vertices_visitados, dicc_padres, dicc_orden):
-    """Funcion auxiliar de depthfirstsearch"""
-
-    dicc_vertices_visitados[origen] = ESTADO_VISITADO
-    for v_ady in grafo.obtener_adyacentes(origen):
-        if dicc_vertices_visitados[v_ady] == ESTADO_NO_VISITADO:
-            dicc_padres[v_ady] = origen
-            dicc_orden[v_ady] = dicc_orden[origen] + 1
-            dfs_visitar(grafo, v_ady, dicc_vertices_visitados, dicc_padres, dicc_orden)
-
-
-def camino_minimo(grafo, origen, destino):
+def camino_minimo(grafo, origen, destino, grado_buscado):
     """Recorre el grafo con BFS para buscar el camino minimo entre dos vertices.
-	Devuelve una lista con el camino, None en caso que no haya camino."""
+    Devuelve un diccionario de vertices con sus correspondientes padres como valor
+    y otro diccionario con vertices como clave y sus grados como valores."""
 
     cola = deque()
     vertices_visitados = {}
     padre_vertices = {}
-    orden_vertices = {}
+    grado_vertices = {}
     cola.append(origen)
     vertices_visitados[origen] = ESTADO_VISITADO
     padre_vertices[origen] = None
-    orden_vertices[origen] = 0
+    grado_vertices[origen] = 0
     while len(cola) > 0:
         v_actual = cola.popleft()
-        if v_actual == destino:
-            return padre_vertices, orden_vertices
+        if v_actual == destino or grado_vertices[v_actual] >= grado_buscado:
+            break
         for v_ady in grafo.obtener_adyacentes(v_actual):
             if v_ady not in vertices_visitados:
                 vertices_visitados[v_ady] = ESTADO_VISITADO
                 padre_vertices[v_ady] = v_actual
-                orden_vertices[v_ady] = orden_vertices[v_actual] + 1
+                grado_vertices[v_ady] = grado_vertices[v_actual] + 1
                 cola.append(v_ady)
-    return (padre_vertices, orden_vertices)
+    return padre_vertices, grado_vertices
 
 
 ###################################################################################
@@ -107,59 +65,45 @@ def grafo_crear(nombre_archivo):
             grafo.agregar_vertice(linea[0])
             for peli in linea[1:]:
                 dicc_pelis[peli][linea[0]] = {}
-    peli = 0
     for pelicula in dicc_pelis:
-        print(peli)
-        peli += 1
         for actor1, actor2 in combinations(dicc_pelis[pelicula], 2):
             grafo.agregar_arista(actor1, actor2, pelicula)
-        dicc_pelis[pelicula] = {}
-        gc.collect()
     return grafo
-
-
-# raise NotImplementedError
 
 
 def camino(grafo, origen, llegada):
     """
-	Devuelve el camino entre un actor de origen y uno de llegada.
+    Devuelve el camino entre un actor de origen y uno de llegada.
 
-	PRE: Recibe el grafo, un actor de origen y un actor de llegada.
-	POST: Devuelve una lista ordenada de cadenas (peliculas) para llegar desde
-	el origen hasta el final.
-	"""
-    padresV, ordenV = camino_minimo(grafo, origen, llegada)
-    recorrid_min = []
+    PRE: Recibe el grafo, un actor de origen y un actor de llegada.
+    POST: Devuelve una lista ordenada de cadenas (peliculas) para llegar desde
+    el origen hasta el final.
+    """
+    padresV, _ = camino_minimo(grafo, origen, llegada, -1)
+    recorrid_min = [llegada]
     v_actual = padresV[llegada]
     while v_actual is not None:
         recorrid_min.append(v_actual)
         v_actual = padresV[v_actual]
     # Revierto la lista de recorrido
     recorrido = list(reversed(recorrid_min))
-    recorrido.append(llegada)
-    if len(recorrido) == 0:
-        return
     lista_de_tuplas = []
     for i in range(len(recorrido) - 1):
-        arista_pelicula = grafo.obtener_aristas(recorrido[i], recorrido[i + 1])[
-            0]  # Como puede que haya mas de una pelicula, agrego la primera
+        arista_pelicula = grafo.obtener_aristas(recorrido[i], recorrido[i + 1])
+        # Como puede que haya mas de una pelicula, agrego la primera
         lista_de_tuplas.append((recorrido[i], recorrido[i + 1], arista_pelicula))
     return lista_de_tuplas
 
 
-# raise NotImplementedError
-
-
 def actores_a_distancia(grafo, origen, n):
     """
-	Devuelve los actores a distancia n del recibido.
+    Devuelve los actores a distancia n del recibido.
 
-	PRE: Recibe el grafo, el actor de origen y el numero deseado.
-	POST: Devuelve la lista de cadenas (actores) a n pasos del recibido.
-	"""
+    PRE: Recibe el grafo, el actor de origen y el numero deseado.
+    POST: Devuelve la lista de cadenas (actores) a n pasos del recibido.
+    """
 
-    dicc_orden = breathfirstsearch(grafo, origen)
+    _, dicc_orden = camino_minimo(grafo, origen, None, n)
     act_n_distantes = []
     for vertice in dicc_orden:
         if dicc_orden[vertice] == n:
@@ -172,22 +116,20 @@ def actores_a_distancia(grafo, origen, n):
 
 def popularidad(grafo, actor):
     """
-	Calcula la popularidad del actor recibido.
+    Calcula la popularidad del actor recibido.
 
-	PRE: Recibe el grafo y un actor de origen
-	POST: Devuelve un entero que simboliza la popularidad: todos los adyacentes
-		de los adyacentes del actor, multiplicado por su cantidad de peliculas
-	"""
+    PRE: Recibe el grafo y un actor de origen
+    POST: Devuelve un entero que simboliza la popularidad: todos los adyacentes
+        de los adyacentes del actor, multiplicado por su cantidad de peliculas
+    """
     if actor not in grafo.vertices:
         return 0
     peliculas_actor = set()
-    actores_grado_dos = set()
-    for actor_adyacente in grafo.vertices[actor]:
-        for actor_grado_dos in grafo.vertices[actor_adyacente]:
-            actores_grado_dos.add(actor_grado_dos)
-        for pelis in grafo.vertices[actor][actor_adyacente]:
+    cantidad_de_actores_grado_dos = len(actores_a_distancia(grafo, actor, GRADO_CONSIDERADO_PARA_POPULARIDAD))
+    for actor_adyacente in grafo.obtener_adyacentes(actor):
+        for pelis in grafo.obtener_aristas(actor, actor_adyacente):
             peliculas_actor.add(pelis)
-    return len(actores_grado_dos) * len(peliculas_actor)
+    return cantidad_de_actores_grado_dos * len(peliculas_actor)
 
 
 def similares(grafo, origen, n):
@@ -199,14 +141,14 @@ def similares(grafo, origen, n):
 	POST: Devuelve una lista de los n actores no adyacentes mas similares al
 		pedido. La lista no debe contener al actor de origen.
 	"""
-    cantidad_de_caminatas = 1000
-    largo_caminata = 35  # Las cantidades estan son arbitrarias, despues vemos que cantidad va mejor.
+    cantidad_de_caminatas = int(len(grafo.obtener_vertices()) / 50)
+    largo_caminata = int(cantidad_de_caminatas / 5)  # Las cantidades estan son arbitrarias, despues vemos que cantidad va mejor.
     actores_similares = defaultdict(int)
 
     for i in range(cantidad_de_caminatas):
         actor_actual = origen
         for j in range(largo_caminata):
-            actor_actual = random.choice(list(grafo.vertices[actor_actual].keys()))
+            actor_actual = random.choice(list(grafo.obtener_adyacentes(actor_actual)))
             actores_similares[actor_actual] += 1
 
     # Saco el actor de origen de el dict de actores_similares
